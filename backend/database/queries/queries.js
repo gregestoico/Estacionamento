5ff9312caf00a743350586bcf2ba4bc7a69fc7c3
manel018 <emanuel.fonseca81@hotmail.com>
2025-02-06 02:15:42 -0300

    inclusão da página de cadastro de clientes

--- backend/database/queries/queries.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    buscarFuncionarioPorEmail: "SELECT * FROM Funcionario WHERE email_func = ? AND senha = ?",
-    inserirEntradaVeiculo: "INSERT INTO Entrada (cpf_cli, placa_veic, hora_entrada) VALUES (?, ?, ?)"
-};
-
